d4b72a241db7548864bf8e3a1d6227c298206b76
catherinekimani <catherinekimani882@gmail.com>
2024-03-11 00:08:34 +0300

    Feat: Implement user auth endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const router = Router();
 
@@ -12,5 +13,14 @@ router.get('/stats', AppController.getStats);
 
 // Endpoint: POST /users
 router.post('/users', UsersController.postNew);
+
+// Endpoint: GET /connect
+router.get('/connect', AuthController.getConnect);
+
+// Endpoint: GET /disconnect
+router.get('/disconnect', AuthController.getDisconnect);
+
+// Endpoint: GET /users/me
+router.get('/users/me', UsersController.getMe);
 
 module.exports = router;
