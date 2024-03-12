7b9dca656a9b9aafe42098ab4d5d2a02075f2a92
catherinekimani <catherinekimani882@gmail.com>
2024-03-13 00:02:49 +0300

    Modify route

--- routes/index.js
+++ routes/index.js
@@ -40,6 +40,6 @@ router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
 // GET /files/:id/data => FilesController.getFile
-router.get('/files:id/data', FilesController.getFile);
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
